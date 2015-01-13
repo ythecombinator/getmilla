6c1f1dad6c8b94e0e8746b0c0a47cd84b09092a9
Matheus Brasil <matheus.brasil10@gmail.com>
2015-01-13 03:44:45 -0200

    Add basic structure

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Milla Theme site's scripts, by @mabrasil 
+
+(function($){
+
+	$(document).ready(function() {
+		
+		// Open window window on click
+		$('.editors a').on('click', function(e) {
+			var mainInner = $('.overlay'),
+				window = $('#' + $(this).attr('data-window'));
+					
+			mainInner.animate({opacity: 0}, 400, function(){
+				$('html,body').scrollTop(0);
+				window.addClass('active').fadeIn(400);
+			});
+			e.preventDefault();
+
+			$('.window-close').on('click', function(e) {
+				window.removeClass('active').fadeOut(400, function(){
+					mainInner.animate({opacity: 1}, 400);
+				});
+				e.preventDefault();
+			});
+		});
+
+		// Tooltips
+		$('.editors a, .share a').tooltip();
+	
+		$('.editors a, .share a').on('click', function () {
+			$(this).tooltip('hide')
+		});
+			
+	});
+})(jQuery);
